1f7f4b4b3232befcbc4de128f50ddf8137dbba94
Student <example@example.com>
2019-06-25 09:53:28 -0700

    added karma and jasmine

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   new UglifyJsPlugin({ sourceMap: true }),
    new CleanWebpackPlugin(['dist']),
    new HtmlWebpackPlugin({
-     title: 'Memory Game',
+     title: 'template',
      template: './src/index.html',
      inject: 'body'
    })
